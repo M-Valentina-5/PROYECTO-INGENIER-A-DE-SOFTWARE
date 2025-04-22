738a518947c91a71a74d974ece06d230d95cd6a7
aliso <aliso@192.168.1.49>
2025-04-22 00:51:00 -0500

    Agregada la carpeta LoginApp completa

--- src/main/java/com/example/loginapp/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.loginapp.repository;
-
-import com.example.loginapp.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmailAndPassword(String email, String password);
-}
-
